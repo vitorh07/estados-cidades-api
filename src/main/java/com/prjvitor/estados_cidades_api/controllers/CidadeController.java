282fbc304fed2bd364c7426fb78d786604b8cdf2
vitorh07 <vitor07devsenai@gmail.com>
2025-04-25 19:05:07 -0300

    busca por id (cidade e estado)

--- src/main/java/com/prjvitor/estados_cidades_api/controllers/CidadeController.java
+++ src/main/java/com/prjvitor/estados_cidades_api/controllers/CidadeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,10 @@ public class CidadeController {
     @GetMapping
     public List<Cidade> listarCidades(){
         return cidadeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Cidade listarCidadePorId(@PathVariable Long id){
+        return cidadeRepository.findById(id).orElse(null);
     }
 }
--- src/main/java/com/prjvitor/estados_cidades_api/controllers/EstadoController.java
+++ src/main/java/com/prjvitor/estados_cidades_api/controllers/EstadoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,10 @@ public class EstadoController {
     @GetMapping
     public List<Estado> listarEstados(){
         return estadoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Estado listarEstadoPorId(@PathVariable Long id){
+        return estadoRepository.findById(id).orElse(null);
     }
 }
